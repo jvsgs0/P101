560f4f5a04c1bf3c0c0ba32b9b93ffbdfd57c836
jvsgs0 <111323425+jvsgs0@users.noreply.github.com>
2024-01-31 19:52:29 -0300

    Add files via upload

---
+++ dado.py
@@ -0,0 +1,44 @@
+import random
+
+response = str(input("Digite y para rolar "))
+
+while response == "y":
+    
+    no = random.randint(1,6)
+    if no == 1:
+        print("[-----]")
+        print("[     ]")
+        print("[  0  ]")
+        print("[     ]")
+        print("[-----]")
+    elif no == 2:
+        print("[-----]")
+        print("[   0 ]")
+        print("[     ]")
+        print("[ 0   ]")
+        print("[-----]")
+    elif no == 3:
+        print("[-----]")
+        print("[   0 ]")
+        print("[  0  ]")
+        print("[ 0   ]")
+        print("[-----]")
+    elif no == 4:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[     ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    elif no == 5:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[  0  ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    elif no == 6:
+        print("[-----]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[ 0 0 ]")
+        print("[-----]")
+    response = str(input("Digite y para ir novamente ou n para parar"))
